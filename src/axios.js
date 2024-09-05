453a1df86708d9bfbbd00fe11711ebcd160126a8
soraya <sorayaeldjama@gmail.com>
2024-09-05 15:44:37 +0200

    11  commit

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,13 @@
-// import axios from "axios";
+import axios from "axios";
 
 // export const makeRequest = axios.create({
 //   baseURL: "http://localhost:8900/api/",
 //   withCredentials: true,
 // });
 
-import axios from "axios";
+// import axios from "axios";
 
 export const makeRequest = axios.create({
-  baseURL: "https://cda-social-media-back-end.onrender.com",
+  baseURL: ["https://cda-social-media-back-end.onrender.com/api",'https://cda-social-media-front-end.vercel.app'],
   withCredentials: true,
 });
