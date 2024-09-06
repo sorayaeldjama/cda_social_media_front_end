f51e2ede944834224d6dfc40f0b32c02d04ab27e
soraya <sorayaeldjama@gmail.com>
2024-09-06 13:31:43 +0200

    17  commit

--- src/axios.js
+++ src/axios.js
@@ -13,16 +13,16 @@ export const makeRequest = axios.create({
 //   withCredentials: true,
 // });
 // Ajouter un intercepteur pour gérer les erreurs
-makeRequest.interceptors.response.use(
-  (response) => response, // Si tout se passe bien, renvoyer la réponse
-  (error) => {
-    if (error.response.status === 401 || error.response.status === 403) {
-      // Supprimer le token si nécessaire
-      document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+// makeRequest.interceptors.response.use(
+//   (response) => response, // Si tout se passe bien, renvoyer la réponse
+//   (error) => {
+//     if (error.response.status === 401 || error.response.status === 403) {
+//       // Supprimer le token si nécessaire
+//       document.cookie = "accessToken=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
       
-      // Rediriger vers la page de login
-      window.location.href = "/login"; // Utilise router.push("/login") si tu utilises Next.js
-    }
-    return Promise.reject(error); // Rejeter l'erreur pour la gestion ailleurs
-  }
-);
+//       // Rediriger vers la page de login
+//       window.location.href = "/login"; // Utilise router.push("/login") si tu utilises Next.js
+//     }
+//     return Promise.reject(error); // Rejeter l'erreur pour la gestion ailleurs
+//   }
+// );
