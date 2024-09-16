e98a629cf19dea4611dd5750cb1e2e692e975aa8
soraya <sorayaeldjama@gmail.com>
2024-09-16 11:13:45 +0200

    commit 1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { useContext } from "react";
 import { createBrowserRouter, RouterProvider, Outlet, Navigate } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import Navbar from "./components/navbar/Navbar";
-import LeftBar from "./components/leftBar/LeftBar";
 import RightBar from "./components/rightBar/RightBar";
 import Home from "./pages/home/Home";
 import Profile from "./pages/profile/Profile";
